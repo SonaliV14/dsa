3c35a4d3884e62b0d4586e86266b67f8f856be09
SonaliV14 <sonalivarshney1404@gmail.com>
2025-06-24 23:38:56 +0530

    Program to reverse a stack

---
+++ reverseStack.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class reverseStack {
+    static class Stack {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        void push(int data) {
+            list.add(data);
+        }
+
+        int pop() {
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        int peek() {
+            return list.get(list.size() - 1);
+        }
+
+        void pushAtBottom(int data) {
+            if (isEmpty()) {
+                push(data);
+                return;
+            }
+
+            int top = pop();
+            pushAtBottom(data);
+            push(top);
+        }
+
+        void reverse() {
+            if (isEmpty()) {
+                return;
+            }
+            int top = pop();
+            reverse();
+            pushAtBottom(top);
+        }
+
+        void printStack() {
+            for (int i = list.size() - 1; i >= 0; i--) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        System.out.println("Original Stack:");
+        s.printStack();
+
+        s.reverse();
+
+        System.out.println("\nReversed Stack:");
+        s.printStack();
+    }
+}
